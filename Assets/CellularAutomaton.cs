6aa55a1bb3d1f84dfd09218286d1cb02170adb9b
fabrizio spadaro <work.fabriziospadaro@gmail.com>
2018-07-25 17:16:48 +0200

    game of life

---
+++ Assets/CellularAutomaton.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+public class CellularAutomaton{
+    public byte[,] cells;
+    public float[,] heat_map;
+    public int sizeX;
+    public int sizeY;
+
+    public CellularAutomaton(int _sizeX,int _sizeY, float fill_percentage){
+        sizeX = _sizeX;
+        sizeY = _sizeY;
+        cells = new byte[sizeX,sizeY];
+        heat_map = new float[sizeX,sizeY];
+        for (int x = 0; x < sizeX; x++)
+            for (int y = 0; y < sizeY; y++)
+                cells[x,y] = Random.Range(0, 100) < fill_percentage ? (byte)1 : (byte)0;
+    }
+
+    public int GetCellValue(int x,int y){
+        return cells[x,y];
+    }
+    public float GetHeatValue(int x, int y){
+        return heat_map[x, y];
+    }
+
+    private int GetNeighbor(int x, int y){
+        int neighbor = 0;
+        for (int j = -1; j < 2; j++)
+            for (int k = -1; k < 2; k++)
+                if (!(j == 0 && k == 0)){
+                    int _x = (x + j + sizeX) % sizeX;
+                    int _y = (y + k + sizeY) % sizeY;
+                    neighbor += cells[_x,_y];
+                }
+
+        return neighbor;
+    }
+
+    public void Step(){
+        byte[,] temp_cells = new byte[sizeX, sizeY];
+        for (int x = 0; x < sizeX; x++)
+            for (int y = 0; y < sizeY; y++){
+                temp_cells[x, y] = GetValueFromRules(x, y);
+                if (cells[x, y] != temp_cells[x, y])
+                    heat_map[x, y] += 0.01f;
+            }
+        
+        cells = temp_cells;
+    }
+
+    private byte GetValueFromRules(int x, int y){
+        byte status = cells[x, y];
+        int n = GetNeighbor(x, y);
+
+        if (status == 0 && n == 3)
+            return 1;
+        else if (status == 1 && (n < 2 || n > 3))
+            return 0;
+        else
+            return status;
+
+    }
+
+}
---
+++ Assets/Scenes/Test.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+    private CellularAutomaton cellular;
+    public Vector2Int size;
+    public float fill;
+    private Color[,] pixel_colors;
+    public Texture2D result;
+	// Use this for initialization
+	void Start () {
+        result = new Texture2D(size.x,size.y);
+        cellular = new CellularAutomaton(size.x,size.y,fill);
+        pixel_colors = new Color[size.x,size.y];
+        SyncColorsWithCells();
+	}
+
+	private void Update(){
+        cellular.Step();
+        SyncColorsWithCells();
+        
+	}
+	void SyncColorsWithCells() {
+        for (int x = 0; x < size.x; x++)
+            for (int y = 0; y < size.y; y++){
+                Color deactive = Color.Lerp(Color.white,Color.grey, cellular.GetHeatValue(x,y)/1.5f);
+                Color active = Color.Lerp(Color.black, Color.green, cellular.GetHeatValue(x, y)/1.5f);
+                Color newcolor = cellular.GetCellValue(x, y) == 1 ? active : deactive;
+                pixel_colors[x, y] = newcolor;
+                result.SetPixel(x, y, pixel_colors[x, y]); 
+            }
+        result.Apply();
+    }
+
+
+	private void OnGUI(){
+        GUI.DrawTexture(new Rect(Screen.width/2-256,Screen.height/2-256,512,512),result);
+	}
+
+}
